96de2024e721c72c24a285ded512aa5438e379cf
nueijod <jedje@aedata.co.kr>
2022-07-13 19:00:33 +0900

    [FCM002] 오류처리 기능 구현 (완료) CODE : toast, dialog 중 선택 가능 TEST : toast형 오류처리- 데이터 불러오기 화면에서 파일 UPLOAD 클릭하여 강제 에러처리 테스트        dialog형 오류처리- 존재하지 않는 API 호출하여 테스트

--- A-rranger-web/src/Custom/MsgDialog.vue
+++ A-rranger-web/src/Custom/MsgDialog.vue
@@ -1,55 +1,36 @@
 <template>
-  <div class="text-center">
-    <v-dialog
-        v-model="dialog"
-        width="300"
-        persistent="true"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-            color="red lighten-2"
-            dark
-            v-bind="attrs"
-            v-on="on"
-        >
-          Click Me
-        </v-btn>
-      </template>
-
+  <div class="container">
+  <v-row justify="center">
       <v-card>
-        <v-card-title class="pa-3 pb-2 red lighten-2">
-          알림
+        <v-card-title class="text-h5">
+          TEST
         </v-card-title>
-        <v-card-text class="pa-5">
-          오류가 발생했습니다.
-        </v-card-text>
-        <v-divider></v-divider>
-
+        <v-card-text>공통으로 사용할 오류 모달입니다.</v-card-text>
         <v-card-actions>
-          <v-spacer></v-spacer>
           <v-btn
-              color="primary"
+              color=" darken-1"
               text
-              @click="dialog = false"
+              @click="$emit('close')"
           >
-            확인
+            Agree
           </v-btn>
         </v-card-actions>
       </v-card>
-    </v-dialog>
+<!--    </v-dialog>-->
+  </v-row>
   </div>
-</template>
-
+  </template>
 <script>
 export default {
-  name: "MsgDialog",
-  data () {
+  data:function(){
     return {
-      dialog: false,
     }
-  },
+  },props : [
+    'hot_table',
+  ],methods : {
+
+  }
 }
 </script>
-
 <style scoped>
 </style>
--- A-rranger-web/src/pages/ProjectList/ProjectList.vue
+++ A-rranger-web/src/pages/ProjectList/ProjectList.vue
@@ -1,10 +1,47 @@
 <template>
   <v-container fluid>
-    <msg-dialog></msg-dialog>
-
+    <div class="tables-basic">
+      <h1 class="page-title mt-10 mb-6">Recent</h1>
+      <v-row>
+        <v-col cols=12>
+          <v-card class="mb-1">
+            <v-card-title class="pa-5 pb-3">
+              <p>Project List</p>
+              <v-spacer></v-spacer>
+            </v-card-title>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                <tr>
+                  <th class="text-left pa-6">MODIFIED</th>
+                  <th class="text-left">NAME</th>
+                  <th class="text-left">NAME</th>
+                  <th class="text-left">TAGS</th>
+                  <th class="text-left">CREATOR</th>
+                  <th class="text-left">SUBJECT</th>
+                  <th class="text-left">DESCRIPTION</th>
+                  <th class="text-left">ROWCOUNT</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(project, index) of projects" :key="index">
+                  <td class="pa-6">>{{ project.modified }}</td>
+                  <td>{{ project.name }}</td>
+                  <td>{{ project.tags }}</td>
+                  <td>{{ project.creator }}</td>
+                  <td>{{ project.subject }}</td>
+                  <td>{{ project.description }}</td>
+                  <td>{{ project.rowcount }}</td>
+                </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
-
 
 <script>
 import { getAllProjectMetas } from '@/api/index'
@@ -12,43 +49,35 @@ import MsgDialog from '@/Custom/MsgDialog'
 
 export default {
   name: 'ProjectList',
-  components: { MsgDialog },
+  // components: { MsgDialog },
   data() {
     return {
-      projectList: [],
-      dialog: false,
+      projects: [],
     }
   },
   created() {
-    this.projectList = [];
     getAllProjectMetas()
         .then((res) => {
-          console.log(res.status);
-          console.log(res.data);
-          console.log('---------------------------1');
-          let json = res.data;
-          let projects = Object.values(json.projects);// project
-          console.log('---------------------------2');
-          console.log(projects);
-          // project 만큼.
-          for (let project of projects) {
-            console.log(project);
-            console.log(project.created);
-            console.log(project.modified);
-            console.log(project.tags);
-          }
+          console.log(res);
+          this.projects = Object.values(res.data.projects);// project
 
-          // keys = Object (res.data.keys);
-          // for (let i = 0; i < keys.length; i++) {
-          //   let key = keys[i];
-          //   console.log("key : " + key + ", value : " + json[key]);
+          // project 만큼
+          // for (let project of projects) {
+          //   console.log(project);
+          //   console.log(project.created);
           // }
-          console.log('---------------------------3');
-          //console.log(res.data.projects.keys().created);
-          console.log('---------------------------4');
 
+          // [FCM002] 오류처리 기능 구현 전역처리
+          // 테스트 방법 : 해당 api call 실패시킴
         }).catch((error) =>{
-          this.dialog = true;
+          this.$modal.show(MsgDialog, {
+            hot_table : 'data',
+            modal : this.$modal },{
+            name: 'dynamic-modal',
+            width : '330px',
+            height : '130px',
+            draggable: true,
+          })
           console.log(error);
     })
   },
