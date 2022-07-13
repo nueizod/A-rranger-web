791da416178e4044104330162a3a5897ce8c638e
Paul Kang <skang@bizflow.com>
2022-07-13 10:46:34 +0900

    Reset Project

--- extensions/wikidata/module/scripts/wikidata-extension-manager.js
+++
@@ -1,6 +0,0 @@
-// Make sure the wikibase manifests are loaded before initializing the schema UI.
-Refine.registerUpdateFunction(function(options) {
-  WikibaseManager.loadWikibases(function () {
-    SchemaAlignment.onProjectUpdate(options);
-  });
-});
